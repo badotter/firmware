edfef2f5ecbd356de883f9237d7e1558ea7d846c
Chris Calef <chris.calef@metabi.com>
2025-06-29 19:41:52 -0700

    Trivial text change.

--- src/graphics/Screen.cpp
+++ src/graphics/Screen.cpp
@@ -163,7 +163,7 @@ static void drawIconScreen(const char *upperMsg, OLEDDisplay *display, OLEDDispl
 
     display->setFont(FONT_MEDIUM);
     display->setTextAlignment(TEXT_ALIGN_LEFT);
-    const char *title = "meshtastic.org";
+    const char *title = "meshtoostic.org";
     display->drawString(x + getStringCenteredX(title), y + SCREEN_HEIGHT - FONT_HEIGHT_MEDIUM, title);
     display->setFont(FONT_SMALL);
 
